b116637c44bca4c2cf31ad2ba0562b404ef793f6
Trystan Newbury <trystan.newbury@thoughtworks.com>
2019-10-29 11:00:49 +0000

    twu-biblioteca: Make the first option appear in the option list for the main menu
    Trystan

--- src/com/twu/biblioteca/App/MainMenu/MainMenu.java
+++ src/com/twu/biblioteca/App/MainMenu/MainMenu.java
@@ -1,4 +1,39 @@
 package com.twu.biblioteca.App.MainMenu;
 
+import com.twu.biblioteca.App.MainMenu.Option.Option;
+
+import java.util.ArrayList;
+
 public class MainMenu {
+    private static ArrayList<Option> optionList = new ArrayList<Option>();
+
+    public static void createListOfOptions() {
+        Option listOfBooksOption = new Option("List of books");
+        optionList.add(listOfBooksOption);
+    }
+
+    public static String showPromptToChooseFromListOfOptions() {
+        return "Please choose from one of the following options:" + "\n";
+    }
+
+    public static String showListOfOptions() {
+        StringBuilder stringContent = new StringBuilder();
+        for (Option option : optionList) {
+            String optionInformation =  "[" + option.getOptionListNumber() + "]: " + option.getTitle();
+            stringContent.append(optionInformation);
+        }
+        return stringContent.toString();
+    }
+
+    public static String selectOptionNumber(int optionNumber) {
+        if (optionNumber == 1) {
+            return Option.showBookChoiceList();
+        } else {
+            return invalidChoiceSelected();
+        }
+    }
+
+    private static String invalidChoiceSelected() {
+        return "Sorry, that's an invalid selection. Please try again.";
+    }
 }
--- test/com/twu/biblioteca/App/MainMenu/Option/OptionTest.java
+++ test/com/twu/biblioteca/App/MainMenu/Option/OptionTest.java
@@ -1,34 +1,38 @@
-package com.twu.biblioteca.App.Option;
+package com.twu.biblioteca.App.MainMenu.Option;
 
-import com.twu.biblioteca.App.Option.Book.Book;
 import org.junit.Test;
-
-import java.util.ArrayList;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasProperty;
 
 public class OptionTest {
-    private ArrayList<Book> createBookList() {
-        ArrayList<Book> bookList = new ArrayList<Book>();
-        Book testBook1 = new Book("1984", "George Orwell", 1949);
-        Book testBook2 = new Book("The Firm", "John Grisham", 1991);
-        bookList.add(testBook1);
-        bookList.add(testBook2);
-        return bookList;
+    private Option createFakeOption() {
+        return new Option("Option One");
     }
 
     @Test
-    public void shouldHaveATitle() {
-        ArrayList<Book> bookList = createBookList();
-        Option newOption = new Option("List of books", bookList);
-        assertThat(newOption.getTitle(), is("List of books"));
+    public void shouldHaveAnIdATitleAndAnAction() {
+        Option newOption = createFakeOption();
+        assertThat(newOption, hasProperty("optionListNumber"));
+        assertThat(newOption, hasProperty("title"));
+    }
+
+    @Test
+    public void shouldHaveCorrectTitleAndId() {
+        Option newOption = createFakeOption();
+        assertThat(newOption.getTitle(), is("Option One"));
+        assertThat(newOption.getOptionListNumber(), is(1));
     }
 
     @Test
     public void shouldShowItsContentWhenSelected() {
-        ArrayList<Book> bookList = createBookList();
-        Option newOption = new Option("List of books", bookList);
-        assertThat(newOption.getContent(), is("1: 1984 - George Orwell | 1949" + "\n" + "2: The Firm - John Grisham | 1991" + "\n"));
+        Option.createListOfBooks();
+        assertThat(Option.showBookChoiceList(), is(
+                "Here is a list of books:" + "\n" + "\n" +
+                "Book List Number: Title - Author | Year Published" + "\n" +
+                "1: 1984 - George Orwell | 1949" + "\n" +
+                "2: The Firm - John Grisham | 1991" + "\n"
+        ));
     }
 }
