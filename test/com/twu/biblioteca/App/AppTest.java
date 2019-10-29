0276022bc9dea6e9679ae90d94021fde6c946a25
Trystan Newbury <trystan.newbury@thoughtworks.com>
2019-10-29 11:02:32 +0000

    twu-bilbioteca: Create a main menu with one option to list our two books that we have.
    Trystan

--- test/com/twu/biblioteca/App/AppTest.java
+++ test/com/twu/biblioteca/App/AppTest.java
@@ -1,16 +1,15 @@
 package com.twu.biblioteca.App;
 
-import com.twu.biblioteca.App.Book.Book;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.*;
-import java.util.ArrayList;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
 
 public class AppTest {
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
@@ -34,20 +33,11 @@ public class AppTest {
     }
 
     @Test
-    public void shouldShowBookListMessage() {
-        String bookListMessage = "Here is a list of books:" + "\n" + "\n" + "Id: Title - Author | Year Published";
-        App.showBookListMessage();
-        assertThat(outContent.toString(), is(bookListMessage + "\n"));
-    }
-
-    @Test
-    public void shouldShowAListOfBooks() {
-        ArrayList<Book> bookList = new ArrayList<Book>();
-        Book testBook1 = new Book("1984", "George Orwell", 1949);
-        Book testBook2 = new Book("The Firm", "John Grisham", 1991);
-        bookList.add(testBook1);
-        bookList.add(testBook2);
-        App.showBookList(bookList);
-        assertThat(outContent.toString(), is("1: 1984 - George Orwell | 1949" + "\n" + "2: The Firm - John Grisham | 1991" + "\n"));
+    public void shouldShowAMainMenu() {
+        try {
+            Class.forName("com.twu.biblioteca.App.MainMenu.MainMenu");
+        } catch (ClassNotFoundException e) {
+            Assert.fail("should render the MainMenu");
+        }
     }
 }
--- test/com/twu/biblioteca/App/MainMenu/MainMenuTest.java
+++ test/com/twu/biblioteca/App/MainMenu/MainMenuTest.java
@@ -1,4 +1,29 @@
 package com.twu.biblioteca.App.MainMenu;
 
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.not;
+
 public class MainMenuTest {
+    @Test
+    public void shouldShowPromptToChooseFromListOfOptions() {
+        String prompt = "Please choose from one of the following options:" + "\n";
+        assertThat(MainMenu.showPromptToChooseFromListOfOptions(), is(prompt));
+    }
+
+    @Test
+    public void shouldShowAListOfOptions() {
+        MainMenu.createListOfOptions();
+        assertThat(MainMenu.showListOfOptions(), is(not("")));
+        assertThat(MainMenu.showListOfOptions(), is("[1]: List of books"));
+    }
+
+    @Test
+    public void shouldReturnAnInvalidChoiceMessageGivenInvalidOptionNumber() {
+        int invalidOptionNumber = 0;
+        assertThat(MainMenu.selectOptionNumber(invalidOptionNumber),
+                is("Sorry, that's an invalid selection. Please try again."));
+    }
 }
